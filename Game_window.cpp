c2a8619b8bca5ec65e2eba58e571e7670212bf7e
MorikawaLin <pengchuanlin1998@gmail.com>
2017-11-09 17:16:12 -0600

    Fixed minor errors

--- Game_window.cpp
+++ Game_window.cpp
@@ -26,6 +26,8 @@ private:
 	Button start_button;
 	Button quit_button;
 
+	void clear();
+
 	static void cb_choose(Address, Address);
 	void choose();
 	static void cb_rule(Address, Address);
@@ -44,8 +46,7 @@ private:
 	static void cb_expr(Address, Address);
 	void expr();
 
-	static void cb_check_and_move(Address, Address);
-	void check_and_move();
+	void check_and_move(int);
 
 	static void cb_quit(Address, Address);
 	void quit();
@@ -54,7 +55,7 @@ private:
 Game_window::Game_window(Point xy, int w, int h, const string& title)
 	:Window{ xy, w, h, title },
 	lev{ 0 },
-	num_labels{ "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"},
+	/*num_labels{ "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"},*/
 	choose_difficulty{ Point{ 30, 450 }, 200, 100, "Choose Difficulty", cb_choose },
 	start_button{ Point{ 260, 430 }, 280, 140, "Start the Game!", cb_start },
 	show_rule{ Point{ 570, 450 }, 200, 100, "Read The Rules", cb_rule },
@@ -111,16 +112,20 @@ void Game_window::cb_expr(Address, Address pw)
 	reference_to<Game_window>(pw).expr();
 }
 
-void Game_window::cb_check_and_move(Address, Address pw) {
-	reference_to<Game_window>(pw).check_and_move();
-}
-
 void Game_window::cb_quit(Address, Address pw)
 {
 	reference_to<Game_window>(pw).quit();
 }
 
+void Game_window::clear() {
+	for (int i = 0; i < children(); ++i) {
+		child(i)->hide();
+	}
+}
+
 void Game_window::choose() {
+	clear();
+
 	attach(beginner);
 	attach(intermediate);
 	attach(advanced);
@@ -129,34 +134,31 @@ void Game_window::choose() {
 	show_rule.move(30 - show_rule.loc.x, 0);
 	attach(back_to_menu);
 	attach(show_rule);
-	detach(choose_difficulty);
+	attach(start_button);
+	attach(quit_button);
 
 	redraw();
 }
 
 void Game_window::rule() {
-	detach(beginner);
-	detach(intermediate);
-	detach(advanced);
-	detach(expert);
+	clear();
 
 	attach(back_to_menu);
 	attach(choose_difficulty);
-	detach(show_rule);
+	attach(start_button);
+	attach(quit_button);
 
 	redraw();
 }
 
 void Game_window::back() {
-	detach(beginner);
-	detach(intermediate);
-	detach(advanced);
-	detach(expert);
+	clear();
 
 	show_rule.move(570 - show_rule.loc.x, 0);
 	attach(choose_difficulty);
 	attach(show_rule);
-	detach(back_to_menu);
+	attach(start_button);
+	attach(quit_button);
 
 	redraw();
 }
@@ -166,20 +168,35 @@ void Game_window::start() {
 		throw;
 	}
 	else {
-		detach(beginner);
-		detach(intermediate);
-		detach(advanced);
-		detach(expert);
+		clear();
 
-		detach(choose_difficulty);
-		detach(show_rule);
-		detach(start_button);
-		detach(back_to_menu);
+		/*for (int i = 0; i < 16; ++i) {
+			numbers.push_back(new Button{ Point{ xs.at(i), ys.at(i) }, 100, 100, num_labels.at(i), [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move();} });
+			attach(numbers[numbers.size() - 1]);
+		}*/
+
+		numbers.push_back(new Button{ Point{ xs.at(0), ys.at(0) }, 100, 100, "", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(0);} });
+		numbers.push_back(new Button{ Point{ xs.at(1), ys.at(1) }, 100, 100, "1", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(1);} });
+		numbers.push_back(new Button{ Point{ xs.at(2), ys.at(2) }, 100, 100, "2", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(2);} });
+		numbers.push_back(new Button{ Point{ xs.at(3), ys.at(3) }, 100, 100, "3", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(3);} });
+		numbers.push_back(new Button{ Point{ xs.at(4), ys.at(4) }, 100, 100, "4", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(4);} });
+		numbers.push_back(new Button{ Point{ xs.at(5), ys.at(5) }, 100, 100, "5", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(5);} });
+		numbers.push_back(new Button{ Point{ xs.at(6), ys.at(6) }, 100, 100, "6", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(6);} });
+		numbers.push_back(new Button{ Point{ xs.at(7), ys.at(7) }, 100, 100, "7", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(7);} });
+		numbers.push_back(new Button{ Point{ xs.at(8), ys.at(8) }, 100, 100, "8", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(8);} });
+		numbers.push_back(new Button{ Point{ xs.at(9), ys.at(9) }, 100, 100, "9", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(9);} });
+		numbers.push_back(new Button{ Point{ xs.at(10), ys.at(10) }, 100, 100, "10", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(10);} });
+		numbers.push_back(new Button{ Point{ xs.at(11), ys.at(11) }, 100, 100, "11", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(11);} });
+		numbers.push_back(new Button{ Point{ xs.at(12), ys.at(12) }, 100, 100, "12", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(12);} });
+		numbers.push_back(new Button{ Point{ xs.at(13), ys.at(13) }, 100, 100, "13", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(13);} });
+		numbers.push_back(new Button{ Point{ xs.at(14), ys.at(14) }, 100, 100, "14", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(14);} });
+		numbers.push_back(new Button{ Point{ xs.at(15), ys.at(15) }, 100, 100, "15", [](Address, Address pw) {reference_to<Game_window>(pw).check_and_move(15);} });
 
 		for (int i = 0; i < 16; ++i) {
-			numbers.push_back(new Button{ Point{ xs.at(i), ys.at(i) }, 100, 100, num_labels.at(i), cb_check_and_move });
-			attach(numbers[numbers.size() - 1]);
+			attach(numbers[i]);
 		}
+
+		attach(quit_button);
 	}
 	redraw();
 }
@@ -208,10 +225,10 @@ void Game_window::expr()
 	lev = 4;
 }
 
-void Game_window::check_and_move() {
+void Game_window::check_and_move(int i) {
 	// Check which button is clicked
 
-	// If the blank button numbers.at(1) is clicked
+	// If the blank button numbers.at(0) is clicked
 	// Hightlight buttons that may move
 	// Act as a hint button
 
